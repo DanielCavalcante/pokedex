08537d0169f4610d1f239634fe67aa19b77fcd78
Daniel Cavalcante <danielcdesouza@gmail.com>
2020-06-25 20:04:04 -0300

    Fix props attribute

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -3,11 +3,12 @@ import "./styles.css";
 import api from "../../services/api";
 
 export default (props) => {
+  const [name, setName] = useState(props.name);
   const [img, setImg] = useState("");
   const [types, setTypes] = useState([]);
   const [type, setType] = useState("");
   useEffect(() => {
-    api.get(`/${props.name}`).then((res) => {
+    api.get(`/${name}`).then((res) => {
       console.log(res.data);
       setImg(res.data.sprites.front_default);
       setTypes(res.data.types);
@@ -24,7 +25,7 @@ export default (props) => {
         <div className={type}>{renderImg()}</div>
       </div>
       <div className="body">
-        <h2>{props.name}</h2>
+        <h2>{name}</h2>
         {types.map((item) => (
           <span className={item.type.name}>{item.type.name}</span>
         ))}
